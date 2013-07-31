cb6c9405203a19c9d41c571d2582753e80edb028
zmontesd <zmontesd@gmail.com>
2013-07-30 18:15:27 -0700

    grab user's data from a github repo, number of commits, and number of forks on that project

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source :rubygems
 
 # PostgreSQL driver
 gem 'pg'
+gem "github_api"
 
 # Sinatra driver
 gem 'sinatra'
--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,12 @@
 get '/' do
-  # Look in app/views/index.erb
+  erb :index
+end
+
+post '/' do 
+  user = params[:username]
+  @repo = params[:reponame]
+  @user_repos = Github.repos.list(user: user)
+  @commits = Github.repos.commits.list(user, @repo)
+  @forks = Github.repos.forks.list(user, @repo)
   erb :index
 end
